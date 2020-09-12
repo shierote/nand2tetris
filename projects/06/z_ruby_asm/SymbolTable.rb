00b4fb6dfb603d111e9dd569c13f61e310054879
shierote <taishi2060@gmail.com>
2020-09-12 13:19:44 +0900

    Create complete assembler

--- projects/06/z_ruby_asm/SymbolTable.rb
+++ projects/06/z_ruby_asm/SymbolTable.rb
@@ -1,8 +1,19 @@
 class SymbolTable
-  attr_accessor :table
+  attr_accessor  :table
 
   def initialize()
-    @table = {}
+    @table = {
+      "SP" => 0,
+      "LCL" => 1,
+      "ARG" => 2,
+      "THIS" => 3,
+      "THAT" => 4,
+      "SCREEN" => 16384,
+      "KBD" => 24576
+    }
+    16.times do |t|
+      @table["R#{t}"] = t
+    end
   end
 
   def addEntry(symbol, address)
--- projects/06/z_ruby_asm/main.rb
+++ projects/06/z_ruby_asm/main.rb
@@ -3,22 +3,30 @@ require './Code'
 require './SymbolTable'
 
 parser = Parser.new(ARGV[0])
+symbolTable = SymbolTable.new()
 puts "=" * 30
 puts parser.lineList
 puts "=" * 30
 
 
+romAddress = 0
 while parser.hasMoreCommands?
   if (parser.commandType == "L_COMMAND")
-    puts parser.symbol
+    symbolTable.addEntry(parser.symbol,romAddress)
+  elsif (parser.commandType == "A_COMMAND" || parser.commandType == "C_COMMAND")
+    romAddress += 1
   end
 
   parser.advance
 end
 
+symbolTable.table.each do |symbol, address|
+  puts "#{symbol}: #{address}"
+end
 puts "=" * 30
 
 parser.currentLineNum = 0
+ramAddressVal = 16
 
 outputFilePath = "./out/#{File.basename(ARGV[0], ".asm")}.hack"
 File.open(outputFilePath, "w") do |f|
@@ -30,15 +38,23 @@ File.open(outputFilePath, "w") do |f|
       puts "  comp: #{parser.comp}"
       puts "  jump: #{parser.jump}"
       binaryLine = "111#{Code.comp(parser.comp)}#{Code.dest(parser.dest)}#{Code.jump(parser.jump)}"
-      puts binaryLine
+      puts "  #{binaryLine}"
       f.puts binaryLine
     elsif (parser.commandType == "A_COMMAND")
       if Parser.isNumber?(parser.symbol)
-        binaryLine = "0#{("%015b" % parser.symbol)}"
-        puts binaryLine
-        f.puts binaryLine
+        aValue = parser.symbol
       else
+        if symbolTable.contains?(parser.symbol)
+          aValue = symbolTable.getAddress(parser.symbol)
+        else
+          symbolTable.addEntry(parser.symbol, ramAddressVal)
+          aValue = symbolTable.getAddress(parser.symbol)
+          ramAddressVal += 1
+        end
       end
+      binaryLine = "0#{("%015b" % aValue)}"
+      puts "  #{binaryLine}"
+      f.puts binaryLine
     end
 
     parser.advance
